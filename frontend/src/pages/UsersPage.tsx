af62663c8ba8a5784a485a396b0c82f9521f4254
abdaljawwadhajras <ajhajras@gmail.com>
2026-07-05 11:52:04 +0300

    accept generic company uuid

--- frontend/src/pages/UsersPage.tsx
+++ frontend/src/pages/UsersPage.tsx
@@ -31,7 +31,7 @@ const EMPTY: UserPayload = {
 };
 
 const STATUSES = ["ACTIVE", "DISABLED", "LOCKED"];
-const UUID_RE = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+const UUID_RE = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
 
 export default function UsersPage() {
   const qc = useQueryClient();
